b5367984727834a8e91e95988c3969a233db5233
Rabbit Spixy <rabbitspixy@gmail.com>
2026-01-10 18:53:36 +0300

    feat: initialize Flutter project with Android, iOS, macOS, Linux, Windows

---
+++ android/app/src/main/kotlin/com/example/touch_of_the_unknown/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.touch_of_the_unknown
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ run.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+set cmd=%~1
+
+if /i "%~1"=="build_runner" (
+    flutter pub run build_runner build --delete-conflicting-outputs
+) else if /i "%~1"=="windows" (
+    flutter run -d windows
+) else if /i "%~1 %~2"=="build windows" (
+    flutter build windows
+) else (
+    echo Unknown command: %cmd%
+    exit /b 1
+)
+
+endlocal
