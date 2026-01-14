12c6d85eaa902b9cebc638cf3686e434db7d3e7f
Rabbit Spixy <rabbitspixy@gmail.com>
2026-01-14 08:42:08 +0300

    feat: add text to editor transitions and update node text

--- lib/editor/editor_state.dart
+++ lib/editor/editor_state.dart
@@ -21,15 +21,24 @@ class EditorState {
       ..y = 0;
     
     final n2 = EditorNode()
-      ..text = "Текст 2"
+      ..text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       ..x = 50
       ..y = 0;
 
     nodes[n1.id] = n1;
     nodes[n2.id] = n2;
-    transitions.add(EditorTransition()..from=n1.id..to=n2.id);
-    transitions.add(EditorTransition()..from=n1.id..to=n2.id);
-    transitions.add(EditorTransition()..from=n1.id..to=n2.id);
+    transitions.add(EditorTransition()
+      ..from=n1.id
+      ..to=n2.id
+      ..text="Переход");
+    transitions.add(EditorTransition()
+      ..from=n1.id
+      ..to=n2.id
+      ..text="Переход");
+    transitions.add(EditorTransition()
+      ..from=n1.id
+      ..to=n2.id
+      ..text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
     transitionsNotifier.value = List.from(transitions);
   }
 }
