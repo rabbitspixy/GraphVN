c2dd42e100549741103a1ea4c6254053aea6bffb
Rabbit Spixy <rabbitspixy@gmail.com>
2026-01-18 13:03:10 +0300

    feat: add arrow indicator to transition painter

--- lib/editor/transition_painter.dart
+++ lib/editor/transition_painter.dart
@@ -67,13 +67,30 @@ class TransitionPainter extends CustomPainter {
         0.25 * start.dy + 0.5 * control.dy + 0.25 * end.dy,
       );
       canvas.drawPath(path, paint);
-      final pointPaint = Paint()..color = Colors.black..style = PaintingStyle.fill;
-      canvas.drawCircle(center, 3, pointPaint);
+      final centerPaint = Paint()
+        ..color = Colors.black
+        ..strokeWidth = 1.5
+        ..strokeCap = StrokeCap.round
+        ..style = PaintingStyle.stroke;
+      final arrow = _arrowPath(center, d.direction);
+      canvas.drawPath(arrow, centerPaint);
       if (transition.id == selectedTransition?.id) {
         final pointPaint = Paint()..color = Colors.black..style = PaintingStyle.stroke;
-        canvas.drawCircle(center, 5, pointPaint);
+        canvas.drawCircle(center, 7, pointPaint);
       }
     }
+  }
+
+  Path _arrowPath(Offset center, double direction) {
+    final l = 5.0;
+    final angle = pi * 0.75;
+    final p2 = center + Offset.fromDirection(direction, l * 0.45);
+    final p1 = p2 + Offset.fromDirection(direction - angle, l);
+    final p3 = p2 + Offset.fromDirection(direction + angle, l);
+    return Path()
+      ..moveTo(p1.dx, p1.dy)
+      ..lineTo(p2.dx, p2.dy)
+      ..lineTo(p3.dx, p3.dy);
   }
 
   @override
