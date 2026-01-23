0c01666c21e277f7692e6093390ba645804a666d
Rabbit Spixy <rabbitspixy@gmail.com>
2026-01-24 01:46:04 +0300

    feat: add edit widget placeholder to struct actions

--- lib/editor/struct_actions.dart
+++ lib/editor/struct_actions.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:rational/rational.dart';
 import 'package:uuid/uuid.dart';
 
@@ -5,10 +6,17 @@ abstract class StructAction {
   String id = Uuid().v4();
   String structId = "";
 
+  Widget edit();
+
   String actionText();
 }
 
 class DoNothing extends StructAction {
+
+  @override
+  Widget edit() {
+    return Text("Not implemented");
+  }
 
   @override
   String actionText() {
@@ -20,6 +28,10 @@ class VariableSetNumberValue extends StructAction {
   String variableId = "";
   Rational newValue = Rational.zero;
 
+  @override
+  Widget edit() {
+    return Text("Not implemented");
+  }
 
   @override
   String actionText() {
@@ -30,6 +42,11 @@ class VariableSetNumberValue extends StructAction {
 class IncreaseNumberValue extends StructAction {
   String variableId = "";
   Rational increaseValue = Rational.zero;
+
+  @override
+  Widget edit() {
+    return Text("Not implemented");
+  }
 
   @override
   String actionText() {
