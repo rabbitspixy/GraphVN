95428da1bee765c095e6b959e81b88f93b4f7cf1
Rabbit Spixy <rabbitspixy@gmail.com>
2026-04-13 01:34:20 +0300

    refactor: remove unused typeId from NamedVariableStringifier

--- lib/editor/named_variable_stringifier.dart
+++ lib/editor/named_variable_stringifier.dart
@@ -1,12 +1,10 @@
 class NamedVariableStringifier {
-  String typeId = "";
   String valueId = "";
   String template = "";
 
   NamedVariableStringifier({
     required this.valueId,
     required this.template,
-    required this.typeId,
   });
 
   String? evaluate(String valueId) {
