cbf80de593bfad5009a249867dbbabc7c5eaf6fa
Rabbit Spixy <rabbitspixy@gmail.com>
2026-04-15 00:12:30 +0300

    feat: add constructor to FirstMatchConditionItem

--- lib/editor/first_match_condition_item.dart
+++ lib/editor/first_match_condition_item.dart
@@ -4,6 +4,11 @@ import 'package:graph_vn/editor/named_value_expression/named_value_expression.da
 class FirstMatchConditionItem {
   NamedValueExpression expression = ConstantNamedValueExpression();
   FirstMatchResult action = FirstMatchResult.pass;
+
+  FirstMatchConditionItem({
+    required this.expression,
+    required this.action,
+  });
 }
 
 enum FirstMatchResult {
