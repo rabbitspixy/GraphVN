17ad9b320dadbed4b8093f760d8b62b3e30c93d8
Rabbit Spixy <rabbitspixy@gmail.com>
2026-08-04 00:35:07 +0300

    Version up

--- lib/app_version.dart
+++ lib/app_version.dart
@@ -1,7 +1,7 @@
 import 'package:pub_semver/pub_semver.dart';
 
 class AppVersion {
-  static final current = Version.parse("0.1.0+1");
+  static final current = Version.parse("0.2.0+2");
 
   static void checkIsSupportedVersion(String version) {
     if (version == "") {
